9ee9aa16d66a4e52ef7e869d50d8e6da86cc6129
Naol Hussien <naol.h@suntecholding.com>
2022-10-06 15:31:00 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Naol Hassen on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
